dfe3b3566a7a751384abdb68243fc3be33f6c446
Koppula Varun <varunkoppula2007@gmail.com>
2026-07-12 13:00:45 +0530

    feat(database): build database seeder and add password field to employee

--- backend/src/models/Employee.js
+++ backend/src/models/Employee.js
@@ -14,6 +14,7 @@ const Employee = sequelize.define('Employee', {
   email: { type: DataTypes.STRING,
      allowNull: false,
       unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
   isActive: { type: DataTypes.BOOLEAN,
      defaultValue: true },
 });
